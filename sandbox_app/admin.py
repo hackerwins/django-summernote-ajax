0f176bd055094d4754ea4cbe457b1c8610584deb
pincoin <pincoins@gmail.com>
2018-01-21 03:15:28 +0900

    - admin attachment filter by post

--- sandbox_app/admin.py
+++ sandbox_app/admin.py
@@ -1,9 +1,36 @@
 from django.contrib import admin
+from django.contrib.admin.filters import SimpleListFilter
 
 from .forms import PostAdminForm
 from .models import (
     Attachment, Post
 )
+
+
+class NullFilterSpec(SimpleListFilter):
+    title = ''
+    parameter_name = ''
+
+    def lookups(self, request, model_admin):
+        return (
+            ('1', 'Has post',),
+            ('0', 'No post',),
+        )
+
+    def queryset(self, request, queryset):
+        kwargs = {
+            '%s' % self.parameter_name: None,
+        }
+        if self.value() == '0':
+            return queryset.filter(**kwargs)
+        if self.value() == '1':
+            return queryset.exclude(**kwargs)
+        return queryset
+
+
+class PostNullFilterSpec(NullFilterSpec):
+    title = 'post'
+    parameter_name = 'post'
 
 
 class AttachmentInline(admin.StackedInline):
@@ -13,6 +40,7 @@ class AttachmentInline(admin.StackedInline):
 
 class AttachmentAdmin(admin.ModelAdmin):
     list_display = ('name', 'post', 'file', 'created')
+    list_filter = (PostNullFilterSpec,)
     ordering = ('-created',)
 
 
