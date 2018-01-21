f85a071e36d945c4e59fe8d84bf31daf02300dbb
pincoin <pincoins@gmail.com>
2018-01-22 03:57:04 +0900

    - read version from project package

---
+++ django_summernote_ajax/__init__.py
@@ -0,0 +1,5 @@
+version_info = (0, 0, 0, 1)
+
+__version__ = VERSION = '.'.join(map(str, version_info))
+__project__ = PROJECT = 'django-summernote-ajax'
+__author__ = AUTHOR = 'django-summernote-ajax contributors'
--- setup.py
+++ setup.py
@@ -4,45 +4,51 @@ from setuptools import (
     setup, find_packages
 )
 
+from django_summernote_ajax import (
+    VERSION, PROJECT, AUTHOR
+)
+
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
-setup(name='django-summernote-ajax',
-      version='0.0.1',
-      packages=find_packages(exclude=['docs', 'sandbox', 'sandbox_app']),
-      description='Django plugin for summernote',
-      long_description=README,
-      url='https://www.pincoin.info/',
-      author='Jonghwa Seo',
-      author_email='mairoo' '@' 'pincoin.info',
-      license='MIT',
+setup(
+    name=PROJECT,
+    version=VERSION,
+    description='Yet another Summernote plugin for Django',
+    long_description=README,
+    author=AUTHOR,
+    maintainer='django-summernote-ajax maintainers',
+    url='http://github.com/summernote/django-summernote-ajax',
+    license='MIT',
 
-      # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Intended Audience :: Developers',
-          'Topic :: Software Development :: Build Tools',
-          'License :: OSI Approved :: MIT License',
-          'Framework :: Django :: 1.8',
-          'Framework :: Django :: 1.11',
-          'Framework :: Django :: 2.0',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-      ],
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 
-      install_requires=[
-          'Django',
-      ],
-      setup_requires=[
-      ],
-      scripts=[
-      ],
+    packages=find_packages(exclude=['docs', 'sandbox', 'sandbox_app']),
 
-      test_suite='tests.get_test_suite',
-      tests_require=[
-      ],
+    install_requires=[
+        'Django',
+    ],
+    setup_requires=[
+    ],
+    scripts=[
+    ],
 
-      zip_safe=False,
-      ),
+    test_suite='tests.get_test_suite',
+    tests_require=[
+    ],
+
+    zip_safe=False,
+),
